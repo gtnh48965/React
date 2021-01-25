68abaa9ad11aed307e7f3ce4bd7575f00f6e3a49
gtnh48965 <gtnh48965@bk.ru>
2021-01-25 04:15:53 +0300

    home 2

---
+++ Two_Work /src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MessageField from './components/MessageField.jsx'
+import Child from './components/Child.jsx'
+
+
+ReactDOM.render(
+    <MessageField />,
+    // <Child />
+    document.getElementById('root')
+)
